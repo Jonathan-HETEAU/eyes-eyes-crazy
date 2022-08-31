633912942591243fa296ad4dbd345e10fab15795
Jonathan HETEAU <jonathan.heteau@gmail.com>
2022-08-31 16:56:03 +0200

    init project

---
+++ src/EyesEyesCrazy.hx
@@ -0,0 +1,12 @@
+import hxd.App;
+
+class EyesEyesCrazy extends hxd.App {
+    
+    override function init() {
+                
+    }
+
+    static function main() {
+        new EyesEyesCrazy();
+    } 
+}
